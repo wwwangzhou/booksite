0448d80c3a2eb394894fb9b22bd65902516ca380
Zach <wang.jaye@yahoo.com>
2019-12-30 16:18:25 -0800

    reviews = [] is no longer needed

--- application.py
+++ application.py
@@ -17,8 +17,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-reviews = []
-
 @app.route("/", methods=["GET", "POST"])
 def index():
     now = datetime.datetime.now()

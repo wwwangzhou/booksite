1140b21cb3c083f2bdfd74df7140a13561984d3a
Zach <wang.jaye@yahoo.com>
2019-12-31 13:16:50 -0800

    added registeration route

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
 import os,datetime, csv
 
-from flask import Flask,render_template, session, request, url_for
+from flask import Flask,render_template, session, request, url_for, flash, redirect
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from forms import RegistrationForm, LoginForm
 
 # bridge the connection between Python and Database
 engine = create_engine(os.getenv("DATABASE_URL"))
@@ -13,6 +14,8 @@ db = scoped_session(sessionmaker(bind=engine))
 
 # create an instance of Flask class
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -97,6 +100,26 @@ def index():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # generate one time alert
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 # to do: user management section
 @app.route("/user")

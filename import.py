4f575baf6679cdbbbbf3f6e446d53e567b403122
Zach <wang.jaye@yahoo.com>
2019-12-30 17:37:36 -0800

    retrive data from database

--- import.py
+++ import.py
@@ -9,11 +9,17 @@ db = scoped_session(sessionmaker(bind=engine))
 
 def main():
     b = open("books_test.csv")
-    reader = csv.reader(b)
+    reader = csv.reader(b) # read b as csv file
+
     for isbn, title, author, year in reader:
         db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
-                    {"isbn": isbn, "title": title, "author": author, "year": year})
+        {"isbn": isbn, "title": title, "author": author, "year": year})
         print(f"Added book title is: {title} written by {author} in {year}.")
+
+    books = db.execute("select * from books where year=1998;").fetchall()
+    for book in books:
+        print(f"Book found: title : {book.title} written by {book.author} in {book.year}.")
+
     db.commit()
 
 if __name__ == "__main__":

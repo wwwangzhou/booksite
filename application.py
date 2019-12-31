cf68ec5e69e0905f34d5a732134893db216cbc86
Zach <wang.jaye@yahoo.com>
2019-12-30 17:48:25 -0800

    从主文件里提取数据 | 假设数据库有东西了

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
-import os,datetime
-
+import os,datetime, csv
 
 from flask import Flask,render_template, session, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 app = Flask(__name__)
 
@@ -16,6 +18,25 @@ if not os.getenv("DATABASE_URL"):
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+def main():
+    # As long as import.py run once, the following is not needed.
+    # b = open("books_test.csv")
+    # reader = csv.reader(b) # read b as csv file
+
+    # for isbn, title, author, year in reader:
+    #     db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)",
+    #     {"isbn": isbn, "title": title, "author": author, "year": year})
+    #     print(f"Added book title is: {title} written by {author} in {year}.")
+
+    books = db.execute("select * from books where year=1998;").fetchall()
+    for book in books:
+        print(f"Book found: title : {book.title} written by {book.author} in {book.year}.")
+
+    db.commit()
+
+if __name__ == "__main__":
+    main()
 
 @app.route("/", methods=["GET", "POST"])
 def index():

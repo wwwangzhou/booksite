14b52413126bed921c6f6b011f834467145a184c
Zach <wang.jaye@yahoo.com>
2019-12-30 18:48:49 -0800

    be careful about the placeholder syntax

--- application.py
+++ application.py
@@ -31,6 +31,23 @@ def main():
     for book in books_found:
         print(f"Book published at 1998 : {book.title} written by {book.author} in {book.year}.")
 
+    # Prompt user to choose a year.
+    year = int(input("\nYear published: "))
+    book = db.execute("SELECT * FROM books WHERE year = :year",
+                        {"year": year}).fetchone()
+
+    # Make sure book is valid.
+    if book is None:
+        print("Error: No such book.")
+    else:
+        # List books found @ variable year.
+        books_found_at_year = db.execute("SELECT * FROM books WHERE year = :year",
+                                {"year": year}).fetchall()
+        for book in books_found_at_year:
+            print(f"Book published at {year} : {book.title} written by {book.author} in {book.year}.")
+        if len(books) == 0:
+            print("No books found.")
+
 if __name__ == "__main__":
     main()
 

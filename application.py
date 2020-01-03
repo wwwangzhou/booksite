1c09aacb950a7cd25cf9ed8be9be2133ec45932e
Zach <wang.jaye@yahoo.com>
2020-01-03 15:03:51 -0800

    第一次听见别人谈起你，你的名字就射在了我的脑壳，仿佛一缕阳光射进了门缝里！

--- application.py
+++ application.py
@@ -36,7 +36,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    posts = db.relationship('Post', backref='author', lazy=True) # it runs a query on the post table and graps all posts posted by this user
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"

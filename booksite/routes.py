2cb5215ddf33e465067db4d7906b572a6dea3492
Zach <wang.jaye@yahoo.com>
2020-01-05 15:12:10 -0800

    deploying

--- booksite/routes.py
+++ booksite/routes.py
@@ -1,6 +1,5 @@
 import os
 import secrets
-from PIL import Image
 from flask import render_template, session, request, url_for, flash, redirect, request, abort
 from booksite import app, db, bcrypt
 from booksite.forms import RegistrationForm, LoginForm, PostForm

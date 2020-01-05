330119ccdb469c6dafd3514222e8675271920a47
Zach <wang.jaye@yahoo.com>
2020-01-05 15:28:08 -0800

    Added heroku db url

--- booksite/__init__.py
+++ booksite/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__) # create an instance of Flask class
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://qqshruedpiwhwp:8682f7032ddda8eb1e3c53b8845a3c024352367165c2f7f4cc908f427831fb0b@ec2-174-129-33-139.compute-1.amazonaws.com:5432/ddtlmm5cjo2b84'
 
 db = SQLAlchemy(app) # For app initialization
 
